5b29b47bd2317cf68bac33b15af04ac060ca4752
Muhammad Owais Aamir <83240869+OwaisAmir@users.noreply.github.com>
2024-12-10 05:55:08 -0800

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,12 @@
+#Comments in python
+
+# single line comment we use hashtag
+
+"""
+And for multiple line comment we use three double quotes 
+print("Day 2 of learning Python")
+
+"""
+
+print("Day 2 of learning Python")
+print("Today i learn how to use single line and multiple line comment")
